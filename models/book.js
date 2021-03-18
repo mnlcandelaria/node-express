48ac2db252dc49f6495b6bbda2cb326322b48100
Manly Candelaria <zmacandelaria@globe.com.ph>
2021-03-19 02:31:30 +0800

    file uplaod db

--- models/book.js
+++ models/book.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
 
-const path = require("path");
-const coverImageBasePath = "uploads/bookCovers";
-
 const bookSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -26,7 +23,11 @@ const bookSchema = new mongoose.Schema({
     required: true,
     default: Date.now,
   },
-  coverImageName: {
+  coverImage: {
+    type: Buffer,
+    required: true,
+  },
+  coverImageType: {
     type: String,
     required: true,
   },
@@ -38,10 +39,15 @@ const bookSchema = new mongoose.Schema({
 });
 
 bookSchema.virtual("coverImagePath").get(function () {
-  if (this.coverImageName != null) {
-    return path.join("/", coverImageBasePath, this.coverImageName);
+  // if (this.coverImageName != null) {
+  //   return path.join("/", coverImageBasePath, this.coverImageName);
+  // }
+
+  if (this.coverImage != null && this.coverImageType != null) {
+    return `data:${
+      this.coverImageType
+    };charset=utf-8;base64,${this.coverImage.toString("base64")}`;
   }
 });
 
 module.exports = mongoose.model("Book", bookSchema);
-module.exports.coverImageBasePath = coverImageBasePath;
--- routes/_books.js
+++ routes/_books.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 const Book = require("../models/book");
 const Author = require("../models/author");
+const uploadPath = path.join("public", Book.coverImageBasePath);
 const imageMimeTypes = ["image/jpeg", "image/png", "image/gif"];
+const upload = multer({
+  dest: uploadPath,
+  fileFilter: (req, file, callback) => {
+    callback(null, imageMimeTypes.includes(file.mimetype));
+  },
+});
 
 // All Books Route
 router.get("/", async (req, res) => {
@@ -39,36 +49,37 @@ router.get("/new", async (req, res) => {
 });
 
 // Create Book Route
-router.post("/", async (req, res) => {
+router.post("/", upload.single("cover"), async (req, res) => {
   // res.send("Create Books");
+  const fileName = req.file != null ? req.file.filename : null;
   const book = new Book({
     title: req.body.title,
     author: req.body.author,
     publishDate: new Date(req.body.publishDate),
-    description: req.body.description,
     pageCount: req.body.pageCount,
+    coverImageName: fileName,
+    description: req.body.description,
   });
-
-  saveCover(book, req.body.cover);
 
   try {
     const newBook = await book.save();
     // res.redirect(`/books/${newBook.id}`);
+    console.log(newBook);
     res.redirect("books");
   } catch (error) {
-    // if (book.coverImageName != null) {
-    //   removeBookCover(book.coverImageName);
-    // }
+    if (book.coverImageName != null) {
+      removeBookCover(book.coverImageName);
+    }
 
     renderNewPage(res, book, true);
   }
 });
 
-// function removeBookCover(fileName) {
-//   fs.unlink(path.join(uploadPath, fileName), (err) => {
-//     if (err) console.error(err);
-//   });
-// }
+function removeBookCover(fileName) {
+  fs.unlink(path.join(uploadPath, fileName), (err) => {
+    if (err) console.error(err);
+  });
+}
 
 async function renderNewPage(res, book, hasError = false) {
   try {
@@ -83,17 +94,6 @@ async function renderNewPage(res, book, hasError = false) {
   } catch (error) {
     console.log(error);
     res.redirect("/books");
-  }
-}
-
-function saveCover(book, coverEncoded) {
-  if (coverEncoded == null) return;
-
-  const cover = JSON.parse(coverEncoded);
-
-  if (cover != null && imageMimeTypes.includes(cover.type)) {
-    book.coverImage = new Buffer.from(cover.data, "base64");
-    book.coverImageType = cover.type;
   }
 }
 
